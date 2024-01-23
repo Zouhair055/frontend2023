361831e1be5bdf4c971f78890a17c63acb97e7f5
DKHISSI Zouhair <zouhair.dkhissi@etu.univ-cotedazur.fr>
2024-01-23 12:02:17 +0100

    change1

--- src/app/shared/data.ts
+++ src/app/shared/data.ts
@@ -1,193 +1,193 @@
 const assignmentsData = [
   {
     "id": 1,
-    "nom": "Devoir Rendu Analyse Arts 15",
-    "dateDeRendu": "2024-10-14T23:17:39.549306Z",
-    "rendu": true,
-    "auteur": "Ivan",
-    "note": 3,
-    "remarques": "Excellent travail",
-    "matiere": "Physique"
+    "nom": "Devoir Rendu Recherche Ing\u00e9nierie des exigences 6",
+    "dateDeRendu": "2024-12-27T12:00:14.833547Z",
+    "rendu": false,
+    "auteur": "Olivia",
+    "note": 14,
+    "remarques": "Bonne utilisation des sources",
+    "matiere": "SGBD"
   },
   {
     "id": 2,
-    "nom": "Devoir Rendu Examen Biologie 5",
-    "dateDeRendu": "2024-08-01T23:17:39.549306Z",
+    "nom": "Devoir Rendu Recherche Ing\u00e9nierie des exigences 1",
+    "dateDeRendu": "2024-03-18T12:00:14.833547Z",
     "rendu": false,
-    "auteur": "Trent",
-    "note": 17,
-    "remarques": "Manque de clart\u00e9",
-    "matiere": "Biologie"
+    "auteur": "Mallory",
+    "note": 19,
+    "remarques": "Bonne analyse",
+    "matiere": "SGBD"
   },
   {
     "id": 3,
-    "nom": "Devoir Rendu Analyse Arts 7",
-    "dateDeRendu": "2024-02-21T23:17:39.549306Z",
+    "nom": "Devoir Rendu Rapport Programmation avanc\u00e9e 3",
+    "dateDeRendu": "2024-10-18T12:00:14.833547Z",
     "rendu": false,
-    "auteur": "Zoe",
-    "note": 19,
-    "remarques": "Hors sujet",
-    "matiere": "FrontEnd"
+    "auteur": "Victor",
+    "note": 1,
+    "remarques": "Excellent travail",
+    "matiere": "Programmation avanc\u00e9e"
   },
   {
     "id": 4,
-    "nom": "Devoir Rendu Soutenance Pagination 17",
-    "dateDeRendu": "2024-06-16T23:17:39.549306Z",
-    "rendu": true,
-    "auteur": "Ursula",
-    "note": 10,
+    "nom": "Devoir Rendu exercice Ing\u00e9nierie des exigences 8",
+    "dateDeRendu": "2024-05-12T12:00:14.833547Z",
+    "rendu": false,
+    "auteur": "Bob",
+    "note": 1,
     "remarques": "Hors sujet",
-    "matiere": "FrontEnd"
+    "matiere": "Ing\u00e9nierie des exigences"
   },
   {
     "id": 5,
-    "nom": "Devoir Rendu Analyse Chimie 4",
-    "dateDeRendu": "2024-06-07T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Walter",
-    "note": 11,
-    "remarques": "Tr\u00e8s cr\u00e9atif",
-    "matiere": "Physique"
+    "nom": "Devoir Rendu Soutenance Programmation avanc\u00e9e 2",
+    "dateDeRendu": "2024-01-28T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Ursula",
+    "note": 7,
+    "remarques": "Manque de sources",
+    "matiere": "SGBD"
   },
   {
     "id": 6,
-    "nom": "Devoir Rendu Examen Biologie 5",
-    "dateDeRendu": "2024-10-22T23:17:39.549306Z",
+    "nom": "Devoir Rendu exercice Ing\u00e9nierie des exigences 8",
+    "dateDeRendu": "2024-06-01T12:00:14.833547Z",
     "rendu": false,
-    "auteur": "Frank",
-    "note": 14,
-    "remarques": "Manque de sources",
-    "matiere": "automatique"
+    "auteur": "Bob",
+    "note": 12,
+    "remarques": "Hors sujet",
+    "matiere": "SGBD"
   },
   {
     "id": 7,
-    "nom": "Devoir Rendu Evaluation Geographie 10",
-    "dateDeRendu": "2024-06-14T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Quentin",
+    "nom": "Devoir Rendu Analyse SGBD 16",
+    "dateDeRendu": "2024-10-17T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Peggy",
     "note": 16,
-    "remarques": "Bonne analyse",
-    "matiere": "Langues"
+    "remarques": "Tr\u00e8s bien structur\u00e9",
+    "matiere": "Communication"
   },
   {
     "id": 8,
-    "nom": "Devoir Rendu rendu Histoire 16",
-    "dateDeRendu": "2024-06-11T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Trent",
-    "note": 19,
-    "remarques": "A am\u00e9liorer",
-    "matiere": "Arts"
+    "nom": "Devoir Rendu Projet Ing\u00e9nierie des exigences 15",
+    "dateDeRendu": "2024-02-21T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Walter",
+    "note": 7,
+    "remarques": "Manque de clart\u00e9",
+    "matiere": "Ing\u00e9nierie des exigences"
   },
   {
     "id": 9,
-    "nom": "Devoir Rendu Soutenance Geographie 9",
-    "dateDeRendu": "2024-11-21T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Sybil",
-    "note": 14,
-    "remarques": "Manque de d\u00e9tails",
-    "matiere": "Histoire"
+    "nom": "Devoir Rendu Soutenance Programmation avanc\u00e9e 2",
+    "dateDeRendu": "2024-03-10T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Ursula",
+    "note": 8,
+    "remarques": "Manque de clart\u00e9",
+    "matiere": "Communication"
   },
   {
     "id": 10,
-    "nom": "Devoir Rendu Etude Mathematiques 2",
-    "dateDeRendu": "2024-04-11T23:17:39.549306Z",
-    "rendu": true,
-    "auteur": "Quentin",
-    "note": 20,
-    "remarques": "Hors sujet",
-    "matiere": "Chimie"
+    "nom": "Devoir Rendu Presentation Ing\u00e9nierie des exigences 13",
+    "dateDeRendu": "2024-11-27T12:00:14.833547Z",
+    "rendu": false,
+    "auteur": "Ursula",
+    "note": 7,
+    "remarques": "Manque de sources",
+    "matiere": "Communication"
   },
   {
     "id": 11,
-    "nom": "Devoir Rendu Synthese automatique 14",
-    "dateDeRendu": "2024-09-13T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Yvonne",
-    "note": 12,
+    "nom": "Devoir Rendu Presentation Ing\u00e9nierie des exigences 13",
+    "dateDeRendu": "2024-02-27T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Olivia",
+    "note": 10,
     "remarques": "Manque de clart\u00e9",
-    "matiere": "technologie"
+    "matiere": "Programmation avanc\u00e9e"
   },
   {
     "id": 12,
-    "nom": "Devoir Rendu Laboratoire Geographie 6",
-    "dateDeRendu": "2024-03-02T23:17:39.549306Z",
-    "rendu": true,
-    "auteur": "Charlie",
-    "note": 8,
-    "remarques": "Hors sujet",
-    "matiere": "FrontEnd"
+    "nom": "Devoir Rendu Projet Ing\u00e9nierie des exigences 12",
+    "dateDeRendu": "2024-05-23T12:00:14.833547Z",
+    "rendu": false,
+    "auteur": "Trent",
+    "note": 12,
+    "remarques": "Manque de d\u00e9tails",
+    "matiere": "Ing\u00e9nierie des exigences"
   },
   {
     "id": 13,
-    "nom": "Devoir Rendu Analyse Arts 7",
-    "dateDeRendu": "2024-11-20T23:17:39.549306Z",
-    "rendu": true,
+    "nom": "Devoir Rendu Recherche Ing\u00e9nierie des exigences 6",
+    "dateDeRendu": "2024-10-08T12:00:14.833547Z",
+    "rendu": false,
     "auteur": "Niaj",
-    "note": 9,
-    "remarques": "Hors sujet",
-    "matiere": "Biologie"
+    "note": 1,
+    "remarques": "Bonne analyse",
+    "matiere": "JavaScript et html"
   },
   {
     "id": 14,
-    "nom": "Devoir Rendu Etude Mathematiques 2",
-    "dateDeRendu": "2024-12-31T23:17:39.549306Z",
+    "nom": "Devoir Rendu Presentation Ing\u00e9nierie des exigences 13",
+    "dateDeRendu": "2024-08-23T12:00:14.833547Z",
     "rendu": false,
-    "auteur": "Heidi",
-    "note": 7,
-    "remarques": "Manque de sources",
-    "matiere": "Physique"
+    "auteur": "Bob",
+    "note": 0,
+    "remarques": "Hors sujet",
+    "matiere": "JavaScript et html"
   },
   {
     "id": 15,
-    "nom": "Devoir Rendu devoir Arts 12",
-    "dateDeRendu": "2024-08-06T23:17:39.549306Z",
+    "nom": "Devoir Rendu Projet Ing\u00e9nierie des exigences 15",
+    "dateDeRendu": "2024-06-17T12:00:14.833547Z",
     "rendu": true,
-    "auteur": "Xavier",
-    "note": 11,
-    "remarques": "Excellent travail",
-    "matiere": "technologie"
+    "auteur": "Niaj",
+    "note": 19,
+    "remarques": "Manque de sources",
+    "matiere": "SGBD"
   },
   {
     "id": 16,
-    "nom": "Devoir Rendu Laboratoire Geographie 6",
-    "dateDeRendu": "2024-09-11T23:17:39.549306Z",
+    "nom": "Devoir Rendu Presentation Communication 9",
+    "dateDeRendu": "2024-02-26T12:00:14.833547Z",
     "rendu": false,
-    "auteur": "Judy",
-    "note": 7,
-    "remarques": "Tr\u00e8s cr\u00e9atif",
-    "matiere": "Philosophie"
+    "auteur": "Alice",
+    "note": 5,
+    "remarques": "Bonne utilisation des sources",
+    "matiere": "Programmation avanc\u00e9e"
   },
   {
     "id": 17,
-    "nom": "Devoir Rendu devoir Arts 12",
-    "dateDeRendu": "2024-09-13T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Charlie",
-    "note": 0,
-    "remarques": "Tr\u00e8s cr\u00e9atif",
-    "matiere": "Biologie"
-  },
-  {
-    "id": 18,
-    "nom": "Devoir Rendu Synthese Pagination 19",
-    "dateDeRendu": "2024-02-25T23:17:39.549306Z",
-    "rendu": false,
-    "auteur": "Quentin",
-    "note": 18,
-    "remarques": "Manque de d\u00e9tails",
-    "matiere": "Pagination"
-  },
-  {
-    "id": 19,
-    "nom": "Devoir Rendu Etude Mathematiques 2",
-    "dateDeRendu": "2024-05-31T23:17:39.549306Z",
+    "nom": "Devoir Rendu Recherche Ing\u00e9nierie des exigences 1",
+    "dateDeRendu": "2024-08-22T12:00:14.833547Z",
     "rendu": true,
     "auteur": "Frank",
     "note": 14,
-    "remarques": "Excellent travail",
-    "matiere": "technologie"
+    "remarques": "Bonne utilisation des sources",
+    "matiere": "Programmation avanc\u00e9e"
+  },
+  {
+    "id": 18,
+    "nom": "Devoir Rendu Presentation Ing\u00e9nierie des exigences 13",
+    "dateDeRendu": "2024-07-09T12:00:14.833547Z",
+    "rendu": false,
+    "auteur": "Yvonne",
+    "note": 19,
+    "remarques": "Bonne utilisation des sources",
+    "matiere": "JavaScript et html"
+  },
+  {
+    "id": 19,
+    "nom": "Devoir Rendu Projet Ing\u00e9nierie des exigences 12",
+    "dateDeRendu": "2024-11-29T12:00:14.833547Z",
+    "rendu": true,
+    "auteur": "Walter",
+    "note": 19,
+    "remarques": "Manque de sources",
+    "matiere": "Communication"
   }
 ];
   export { assignmentsData };
